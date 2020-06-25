977e67034300a00acee87ec01b24292dcf01aec5
Taylor Howard <tdhoward50@gmail.com>
2020-06-25 11:50:41 -0500

    Initial Commit

---
+++ ScienceScenterIOS14/ScienceScenterIOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  ScienceScenterIOS14App.swift
+//  ScienceScenterIOS14
+//
+//  Created by Taylor Howard on 6/25/20.
+//
+
+import SwiftUI
+
+@main
+struct ScienceScenterIOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
